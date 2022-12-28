f75b2f73cee7cd71e2a5e14f9281db5c17146554
mvasquezg <ing.mvasquezg@gmail.com>
2022-12-28 10:25:55 -0600

    Pagina03.-Creando el controller y el modelo, para la aplicacion de agregar mas alumnos a la libreta

---
+++ cursoDeAngularJsJesusConde/js/StudenController.js
@@ -0,0 +1,17 @@
+var app=angular.module('studentApp', []);
+            
+app.controller('ControllerStudent', function($scope){
+    $scope.arrayStudents=[
+        {name: "Miguel Vasquez", phone: "12345", course: "4to Redes"},
+        {name: "Ramon Verdejo", phone: "67890", course: "Ing Civil"},
+        {name: "Janeth Pech", phone: "23456", course: "Derecho"},
+        {name: "Guadalupe Esquivel", phone: "78901", course: "Contabilidad"},
+        {name: "Roman Arias", phone: "34567", course: "Diseño Gráfico"}
+    ];
+
+    $scope.saveStudent=function(){
+        $scope.arrayStudents.push({name:$scope.newStudent.name, phone:$scope.newStudent.phone, course:$scope.newStudent.course});
+    }
+
+});
+
